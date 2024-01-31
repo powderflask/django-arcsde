42504343300007f810e18e1725f7b45d3a1ae4fc
powderflask <powderflask@gmail.com>
2024-01-30 19:02:39 -0800

    bumpver 1.3.3

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='arcsde.tests',
     tests_require=test_requirements,
     url='https://github.com/powderflask/django-arcsde',
-    version='1.3.2',
+    version='1.3.3',
     zip_safe=False,
 )
